ec85b99bb583b3db30697616a3d25d84fbba10ec
jakecopp <jacob.copp@unh.edu>
2024-06-20 00:30:17 -0400

    FAQ additions

--- public/scripts/oro.js
+++ public/scripts/oro.js
@@ -33,5 +33,14 @@ oro.toggleTopBar = function () {
     }
 }
 
+oro.accordionDrop = function (id) {
+	var x = document.getElementById(id);
+	if (x.className.indexOf("w3-show") == -1) {
+		x.className += " w3-show";
+	} else {
+		x.className = x.className.replace(" w3-show", "");
+	}
+}
+
 window.onscroll = function() {oro.toggleTopBar()};
 window.onresize = function() {oro.setPaddingHeight()};
