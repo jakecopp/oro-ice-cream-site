04a9fb464cf89c684b93e5d65b9ba3e5ff907589
jakecopp <jacob.copp@unh.edu>
2024-07-21 18:05:01 -0400

    Menubar Stickiness overhaul and padding changes

--- public/scripts/oro.js
+++ public/scripts/oro.js
@@ -6,12 +6,6 @@ var topBar = document.getElementById("topBar");
 var afterTop = document.getElementById("afterTop"); //header
 var sticky = topBar.offsetTop;
 
-var topPad = parseInt(afterTop.style.paddingTop);
-var headingHeight = parseInt(afterTop.style.height);
-
-var newHeadingHeight = topBar.offsetHeight + headingHeight + "px";
-var padingHeight = topBar.offsetHeight + topPad + "px";
-
 
 oro.toggleMobileNavMenu = function () {
     var x = document.getElementById("mobileNav");
@@ -22,19 +16,11 @@ oro.toggleMobileNavMenu = function () {
     }
 }
 
-oro.setPaddingHeight = function () {
-    padingHeight = document.getElementById('topBar').offsetHeight + topPad + "px"
-}
-
 oro.toggleTopBar = function () {
     if (window.pageYOffset > sticky) {
         topBar.classList.add("w3-top");
-        afterTop.style.paddingTop = padingHeight;
-        afterTop.style.height = newHeadingHeight;
     } else {
         topBar.classList.remove("w3-top");
-        afterTop.style.paddingTop = topPad + "px";
-        afterTop.style.height = headingHeight + "px";
     }
 }
 
@@ -48,4 +34,3 @@ oro.accordionDrop = function (id) {
 }
 
 window.onscroll = function() {oro.toggleTopBar()};
-window.onresize = function() {oro.setPaddingHeight()};
